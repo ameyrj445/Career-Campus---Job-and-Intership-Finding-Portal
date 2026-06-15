8be5dfc7170a194ce6e6e1ce4a57b0694ee6b523
ameyrj445 <jainameyraj@gmail.com>
2026-06-15 11:23:13 +0530

    version v4

--- backend/dataLoader.js
+++ backend/dataLoader.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const extrasDir = path.resolve(__dirname, '../extras');
-
+                                  
 const jobColumns = [
     'id',
     'title_and_role',
