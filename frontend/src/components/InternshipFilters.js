7da10c2d8d699c5905908739a9801558f012598b
ameyrj445 <jainameyraj@gmail.com>
2026-08-14 09:15:05 +0530

    version v4

--- frontend/src/components/InternshipFilters.js
+++ frontend/src/components/InternshipFilters.js
@@ -285,17 +285,17 @@ function InternshipFilters() {
           />
         </div>
 
-        <div className="filter-buttons"> 
-          <button type="submit" className="apply-filters"> 
-            Apply Filters 
-          </button> 
-          <button type="button" onClick={handleClear} className="clear-filters"> 
-            Clear Filters 
-          </button> 
-        </div> 
-      </form> 
-    </div> 
-  );
-}
+        <div className="filter-buttons">  
+          <button type="submit" className="apply-filters">  
+            Apply Filters   
+          </button>  
+          <button type="button" onClick={handleClear} className="clear-filters">  
+            Clear Filters  
+          </button>  
+        </div>  
+      </form>  
+    </div>  
+  ); 
+} 
 
-export default InternshipFilters;
+export default InternshipFilters; 
